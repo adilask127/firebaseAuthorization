731e10bf82869aaa3485de32b44cd6f1b977a3f5
Adil_Askar <adilask@gmail.com>
2021-12-06 00:42:49 +0600

    Initial commit

---
+++ src/app/hooks/useAuth.jsx
@@ -0,0 +1,114 @@
+import React, { useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import userService from "../services/user.service";
+import { toast } from "react-toastify";
+import { setTokens } from "../services/localStorage.service";
+
+const httpAuth = axios.create();
+
+const AuthContext = React.createContext();
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+const AuthProvider = ({ children }) => {
+    const [currentUser, setUser] = useState({});
+    const [error, setError] = useState(null);
+
+    async function signUp({ email, password, ...rest }) {
+        // const keyFireBasePrivate = "AIzaSyBrRV1ke3unu6m5GLVzWuD2UHQ50EPbI60";
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`;
+        try {
+            const { data } = await httpAuth.post(url, {
+                email,
+                password,
+                returnSecureToken: true
+            });
+            setTokens(data);
+            await createUser({ _id: data.localId, email, ...rest });
+        } catch (error) {
+            errorCatcher(error);
+            const { code, message } = error.response.data.error;
+            if (code === 400) {
+                if (message === "EMAIL_EXISTS") {
+                    const errorObject = {
+                        email: "Ползователь с таким Email уже существует"
+                    };
+                    throw errorObject;
+                }
+            }
+            // throw new Error();
+        }
+    }
+
+    async function signIn({ email, password, ...rest }) {
+        // const keyFireBasePrivate = "AIzaSyBrRV1ke3unu6m5GLVzWuD2UHQ50EPbI60";
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_KEY}`;
+        try {
+            const { data } = await httpAuth.post(url, {
+                email,
+                password,
+                returnSecureToken: true
+            });
+            console.log("--", data);
+            setTokens(data);
+            // await getUser({ _id: data.localId, email, ...rest });
+        } catch (error) {
+            errorCatcher(error);
+            const { code, message } = error.response.data.error;
+            console.log(code, message);
+            if (code === 400) {
+                if (message === "EMAIL_NOT_FOUND") {
+                    const errorObject = {
+                        email: "Ползователь с таким Email не найден"
+                    };
+                    throw errorObject;
+                }
+                if (message === "INVALID_PASSWORD") {
+                    const errorObject = {
+                        password: "Пароль не верный"
+                    };
+                    throw errorObject;
+                }
+            }
+            // throw new Error();
+        }
+    }
+
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+    }
+
+    async function createUser(data) {
+        try {
+            const { content } = userService.create(data);
+            setUser(content);
+        } catch (error) {
+            errorCatcher(error);
+        }
+    }
+    return (
+        <AuthContext.Provider value={{ signUp, currentUser, signIn }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
+
+export default AuthProvider;
